8b61729404f259ffe380fdf996247184f68e1f86
RAGNOARAKNOS <ragnocorp@googlemail.com>
2015-11-27 22:53:56 +0000

    Refactor marvel connector

---
+++ Marvelous/Basic/MarvelApiConnector.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic
+{
+    internal class MarvelApiConnector
+    {
+        private HttpClient apiClient;
+        private Tuple<string, string> apiKeys;
+
+        internal MarvelApiConnector(string privateApi, string publicApi, string address)
+        {
+            this.apiClient = new HttpClient();
+            this.SetupApiClient(address);
+            this.apiKeys = new Tuple<string, string>(privateApi, publicApi);
+        }
+
+        public string PublicKey
+        {
+            get
+            {
+                return this.apiKeys.Item2;
+            }
+        }
+
+        public string PrivateKey
+        {
+            get
+            {
+                return this.apiKeys.Item1;
+            }
+        }
+
+        private void SetupApiClient(string address)
+        {
+            this.apiClient.BaseAddress = new Uri(address);
+            this.apiClient.DefaultRequestHeaders.Accept.Clear();
+            this.apiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        private async Task<string> GenerateHash()
+        {
+            return await Task.Run(() => this.GenerateHashTest());
+        }
+
+        private string GenerateHashTest()
+        {
+            // Create the sum key
+            string sumString = this.GenerateTimespan() + this.PrivateKey + this.PublicKey;
+
+            // Create MD5 crypto encoder
+            MD5 md5 = MD5.Create();
+
+            byte[] stringMap = Encoding.UTF8.GetBytes(sumString);
+            byte[] hash = md5.ComputeHash(stringMap);
+
+            StringBuilder hexHash = new StringBuilder();
+            for (int i = 0; i < hexHash.Length; i++)
+            {
+                // Format the string into hexadecimal
+                hexHash.Append(hash[i].ToString("x2"));
+            }
+
+            return hexHash.ToString();
+        }
+
+        private string GenerateTimespan()
+        {
+            return ((int)(DateTime.Now.ToUniversalTime() - new DateTime(1970, 1, 1)).TotalSeconds).ToString();
+        }
+
+        private string GenerateRequestString()
+        {
+
+        }
+    }
+}
--- Marvelous/MarvelRabbit/Program.cs
+++ Marvelous/MarvelRabbit/Program.cs
@@ -13,21 +13,25 @@ namespace MarvelRabbit
     {
         public static void TX()
         {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+
             using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare("hello", false, false, false, null);
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
 
-                string message = "Hello World!";
-                var body = Encoding.UTF8.GetBytes(message);
+                    string message = "Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
 
-                channel.BasicPublish("", "hello", null, body);
-                Console.WriteLine(" [x] Sent {0}", message);
+                    channel.BasicPublish("", "hello", null, body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
             }
-
-            Console.WriteLine(" Press [enter] to exit.");
-            Console.ReadLine();
         }
     }
     class Receive
